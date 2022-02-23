5a09ca15ea4ca4a34a36796197b48b34e76e37fc
Robin Freyler <robin.freyler@gmail.com>
2022-02-23 15:12:02 +0100

    enhance closure_tree approach

--- src/closure_tree.rs
+++ src/closure_tree.rs
@@ -3,37 +3,95 @@
 #[cfg(test)]
 use crate::benchmark;
 
-use super::{handler, Bits, Context, Outcome, Register};
+use super::{handler, Bits, Context, Outcome};
+
+#[derive(Copy, Clone)]
+pub struct Register(usize);
 
 /// A closure based expression.
 pub struct Expr {
-    handler: Box<dyn Fn(&mut Context) -> Option<Bits>>,
+    handler: Box<dyn Fn(&mut Context) -> Bits>,
+}
+
+pub enum Input {
+    Register(Register),
+    Immediate(Bits),
+    Expr(Expr),
+}
+
+impl From<Register> for Input {
+    fn from(register: Register) -> Self {
+        Self::Register(register)
+    }
+}
+
+impl From<Bits> for Input {
+    fn from(bits: Bits) -> Self {
+        Self::Immediate(bits)
+    }
+}
+
+impl From<Expr> for Input {
+    fn from(expr: Expr) -> Self {
+        Self::Expr(expr)
+    }
 }
 
 impl Expr {
     /// Executes the given expression using the given [`Context`].
-    pub fn execute(&self, context: &mut Context) -> Option<Bits> {
+    pub fn execute(&self, context: &mut Context) -> Bits {
         (self.handler)(context)
     }
 
     /// Creates a new [`Inst`] from the given closure.
     fn new<T>(handler: T) -> Self
     where
-        T: Fn(&mut Context) -> Option<Bits> + 'static,
+        T: Fn(&mut Context) -> Bits + 'static,
     {
         Self {
             handler: Box::new(handler),
         }
     }
 
-    /// Subtracts the constant `imm` from the contents of `src` and return the result.
-    pub fn sub_imm(result: Register, src: Register, imm: Bits) -> Self {
+    pub fn add<P0, P1>(result: Register, lhs: P0, rhs: P1) -> Self
+    where
+        P0: Eval + 'static,
+        P1: Eval + 'static,
+    {
         Self::new(move |context| {
-            let lhs = context.get_reg(src);
-            let rhs = imm;
-            let eval = lhs.wrapping_sub(rhs);
-            context.set_reg(result, eval);
-            Some(eval)
+            let lhs = lhs.eval(context);
+            let rhs = rhs.eval(context);
+            let new_value = lhs.wrapping_add(rhs);
+            context.set_reg(result.0, new_value);
+            new_value
+        })
+    }
+
+    pub fn sub<P0, P1>(result: Register, lhs: P0, rhs: P1) -> Self
+    where
+        P0: Eval + 'static,
+        P1: Eval + 'static,
+    {
+        Self::new(move |context| {
+            let lhs = lhs.eval(context);
+            let rhs = rhs.eval(context);
+            let new_value = lhs.wrapping_sub(rhs);
+            context.set_reg(result.0, new_value);
+            new_value
+        })
+    }
+
+    pub fn mul<P0, P1>(result: Register, lhs: P0, rhs: P1) -> Self
+    where
+        P0: Eval + 'static,
+        P1: Eval + 'static,
+    {
+        Self::new(move |context| {
+            let lhs = lhs.eval(context);
+            let rhs = rhs.eval(context);
+            let new_value = lhs.wrapping_mul(rhs);
+            context.set_reg(result.0, new_value);
+            new_value
         })
     }
 }
@@ -42,6 +100,28 @@ impl Expr {
 pub struct Inst {
     /// The closure stores everything required for the instruction execution.
     handler: Box<dyn Fn(&mut Context) -> Outcome>,
+}
+
+pub trait Eval {
+    fn eval(&self, context: &mut Context) -> Bits;
+}
+
+impl Eval for Register {
+    fn eval(&self, context: &mut Context) -> Bits {
+        context.get_reg(self.0)
+    }
+}
+
+impl Eval for Bits {
+    fn eval(&self, context: &mut Context) -> Bits {
+        *self
+    }
+}
+
+impl Eval for Expr {
+    fn eval(&self, context: &mut Context) -> Bits {
+        self.execute(context)
+    }
 }
 
 impl Inst {
@@ -60,20 +140,28 @@ impl Inst {
         }
     }
 
-    /// Adds the constant `imm` and the contents of `src` and stores the result into `result`.
-    pub fn add_imm(result: Register, src: Register, imm: Bits) -> Self {
-        Self::new(move |context| handler::add_imm(context, result, src, imm))
+    pub fn exec(expr: Expr) -> Self {
+        Self::new(move |context| {
+            expr.eval(context);
+            Outcome::Continue
+        })
     }
 
-    /// Subtracts the constant `imm` from the contents of `src` and stores the result into `result`.
-    pub fn sub_imm(result: Register, src: Register, imm: Bits) -> Self {
-        Self::new(move |context| handler::sub_imm(context, result, src, imm))
+    pub fn local_set<I>(result: Register, input: I) -> Self
+    where
+        I: Eval + 'static,
+    {
+        Self::new(move |context| {
+            let new_value = input.eval(context);
+            context.set_reg(result.0, new_value);
+            Outcome::Continue
+        })
     }
 
     /// Branches to the instruction indexed by `target` if the contents of `condition` are zero.
     pub fn branch_eqz(condition: Expr) -> Self {
         Self::new(move |context| {
-            let condition = condition.execute(context).unwrap();
+            let condition = condition.execute(context);
             if condition == 0 {
                 Outcome::Return
             } else {
@@ -83,9 +171,13 @@ impl Inst {
     }
 
     /// Executes all the instructions in the basic block one after another.
-    pub fn basic_block(insts: Vec<Inst>) -> Self {
+    pub fn basic_block<I>(insts: I) -> Self
+    where
+        I: IntoIterator<Item = Inst>,
+    {
+        let insts = insts.into_iter().collect::<Vec<_>>().into_boxed_slice();
         Self::new(move |context| {
-            for inst in &insts {
+            for inst in &insts[..] {
                 match inst.execute(context) {
                     Outcome::Continue => (),
                     Outcome::Return => return Outcome::Return,
@@ -107,7 +199,7 @@ impl Inst {
 
     /// Returns execution of the function and returns the result in `result`.
     pub fn ret(result: Register) -> Self {
-        Self::new(move |context| handler::ret(context, result))
+        Self::new(move |context| handler::ret(context, result.0))
     }
 }
 
@@ -115,8 +207,8 @@ impl Inst {
 fn counter_loop() {
     let repetitions = 100_000_000;
     let inst = Inst::basic_block(vec![
-        Inst::add_imm(0, 0, repetitions),
-        Inst::loop_block(Inst::branch_eqz(Expr::sub_imm(0, 0, 1))),
+        Inst::exec(Expr::add(Register(0), Register(0), repetitions)),
+        Inst::loop_block(Inst::branch_eqz(Expr::sub(Register(0), Register(0), 1))),
     ]);
     let mut context = Context::default();
     benchmark(|| inst.execute(&mut context));
